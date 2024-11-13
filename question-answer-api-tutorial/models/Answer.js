7a156c62a827283549c6d9515c39ea8c66250fb3
Melih Canaz <92990734+Melihcnz@users.noreply.github.com>
2024-11-13 12:30:35 +0300

    Answer.js

--- question-answer-api-tutorial/models/Answer.js
+++ question-answer-api-tutorial/models/Answer.js
@@ -6,7 +6,7 @@ const Schema = mongoose.Schema;
 const AnswerSchema = new Schema({
     content : {
         type : String,
-        required: [true,"Please provide a conten"],
+        required: [true,"Please provide a content"],
         minlenght : [10,"Please provide a content at least 10 characters"]
     },
     createdAt : {
